ed616a7b4b993cfa4b47838ce0426018bb759024
Shin <s7j7h7@gmail.com>
2023-01-23 05:58:10 +0900

    kakao sharing add

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:kakao_flutter_sdk/kakao_flutter_sdk.dart';
 import 'package:partyone/screens/login_screen.dart';
 
 void main() {
+  // 웹 환경에서 카카오 로그인을 정상적으로 완료하려면 runApp() 호출 전 아래 메서드 호출 필요
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // runApp() 호출 전 Flutter SDK 초기화
+  KakaoSdk.init(
+    nativeAppKey: '417cc2e76cac3d146bd0fcb7092162b4',
+    javaScriptAppKey: '139214de4691bbf379d084050fa68d30',
+  );
+
   runApp(const App());
 }
 
--- lib/widget/market_url_share.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_share/flutter_share.dart';
-
-Future<void> marketUrlShare() async {
-  await FlutterShare.share(
-      title: 'Party @ne',
-      text: '당신 주변의 파티에 참여하세요',
-      linkUrl: 'https://www.google.com/',
-      chooserTitle: 'Example Chooser Title');
-}
